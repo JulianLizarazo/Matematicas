7334543c7bda81f09278a42d64624c62f60010dd
Julian Lizarazo <julianca20122001@hotmail.com>
2021-07-14 13:36:15 -0500

    Circle page working succesfully (javascript code added)

---
+++ js/AreasyPerimetros/Circle.js
@@ -0,0 +1,31 @@
+function areaCircle(radius){
+    return Math.PI * Math.pow(radius,2);
+}
+
+function resultAreaCircle(){
+    var inputRadius = document.getElementById("InputRadio");
+
+    var valueRadius = parseInt(inputRadius.value);
+
+    var area = areaCircle(valueRadius);
+
+    var text = document.getElementById("ResultAreaCircle");
+
+    text.innerText = "El area es de " + area.toFixed(2) + " m²";
+}
+
+function perimeterCircle(radius){
+    return 2*Math.PI*radius;
+}
+
+function resultPerimeterCircle(){
+    var inputRadius = document.getElementById("InputRadioP");
+
+    var valueRadius = parseInt(inputRadius.value);
+
+    var perimeter = perimeterCircle(valueRadius);
+
+    var text = document.getElementById("ResultPerimeterCircle");
+
+    text.innerText = "El perimetro es de " + perimeter.toFixed(2) + " m";
+}
