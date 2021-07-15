41797e972e1e62d192a0e1014615033a307075d1
Julian Lizarazo <julianca20122001@hotmail.com>
2021-07-14 22:57:33 -0500

    Median section working succesfully

--- js/Promedios/Median.js
+++ js/Promedios/Median.js
@@ -1,3 +1,34 @@
 function median(medianArray){
+    var evenArray = parseInt(medianArray.length / 2);
+    let median;
+
+    if(medianArray.length % 2 === 0 ){
+        var element1 = medianArray[evenArray - 1];
+        var element2 = medianArray[evenArray];
+
+        median = (element1 + element2) / 2;
+    } else {
+       median = medianArray[evenArray]; 
+    }
+
+    return median;
+}
+
+function resultMedian(){
+
+    var inputAverage = document.getElementById("InputMMM").value;
     
+    var arrayValue = new Array();
+
+    arrayValue = inputAverage.split(",").map(i=>Number(i));
+    
+    arrayValue = arrayValue.sort(function(a,b){
+        return a-b;
+    });
+
+    var medianValue = median(arrayValue);
+
+    var text = document.getElementById("ResultMMM");
+
+    text.innerText = "La mediana es: " + medianValue;
 }
--- js/Promedios/js/Median.dev.js
+++ js/Promedios/js/Median.dev.js
@@ -1,3 +1,30 @@
 "use strict";
 
-function median(medianArray) {}
+function median(medianArray) {
+  var evenArray = parseInt(medianArray.length / 2);
+  var median;
+
+  if (medianArray.length % 2 === 0) {
+    var element1 = medianArray[evenArray - 1];
+    var element2 = medianArray[evenArray];
+    median = (element1 + element2) / 2;
+  } else {
+    median = medianArray[evenArray];
+  }
+
+  return median;
+}
+
+function resultMedian() {
+  var inputAverage = document.getElementById("InputMMM").value;
+  var arrayValue = new Array();
+  arrayValue = inputAverage.split(",").map(function (i) {
+    return Number(i);
+  });
+  arrayValue = arrayValue.sort(function (a, b) {
+    return a - b;
+  });
+  var medianValue = median(arrayValue);
+  var text = document.getElementById("ResultMMM");
+  text.innerText = "La mediana es: " + medianValue;
+}
